26e243ec2732f1e7d2b5b91804f4b1a4f1cd829a
Asier-R <monstruosaurio@gmail.com>
2024-03-09 09:48:26 +0100

    Se añade prueba y TODOs recordatorios

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Hospital!");
+
+        List<String> lista = new ArrayList<>(List.of("uno","dos"));
+        lista.add(1,"X");
+        lista.forEach(System.out::println);
+
+        // TODO-ASIER: crear CSVs (desordenados) para los datos a insertar en el pre-inicio de la aplicación.
+        // TODO-ASIER: implementar Hashmap<String, Integer> para dar pesos a los caracteres.
+        // TODO-ASIER: revisar si es necesario implementar un comparator o con el hashmap solo es suficiente.
+        // TODO-ASIER: los datos se insertan de forma ordenada en listas.
+
     }
 }
