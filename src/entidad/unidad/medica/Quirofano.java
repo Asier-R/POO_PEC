70113cab8fb58f98caa5dfa7c6877ada77d7464d
Asier-R <monstruosaurio@gmail.com>
2024-03-30 10:04:45 +0100

    Se implementan quirofano y urgencias

--- src/entidad/unidad/medica/Quirofano.java
+++ src/entidad/unidad/medica/Quirofano.java
@@ -1,3 +1,37 @@
 package entidad.unidad.medica;
 
-public class Quirofano {}
+import enumerado.CodigoActividadEnum;
+import enumerado.CodigoAreaEnum;
+
+/** Subclase de {@link Medica} que representa un quirófano del hospital. */
+public class Quirofano extends Medica {
+
+  /** Código de la actividad que se realiza en la unidad. */
+  private CodigoActividadEnum codigoActividad;
+
+  /**
+   * Constructor de Quirófano.
+   *
+   * @param nombre nombre de la unidad.
+   */
+  public Quirofano(String nombre) {
+    super(CodigoAreaEnum.OPERATORIO, nombre);
+    this.codigoActividad = CodigoActividadEnum.QUIROFANO;
+  }
+
+  @Override
+  public CodigoActividadEnum getCodigoActividad() {
+    return this.codigoActividad;
+  }
+
+  @Override
+  public String toString() {
+    return this.getCodigoUnidad()
+        + "-"
+        + this.getCodigoUnidad()
+        + "-"
+        + this.getCodigoActividad()
+        + "-"
+        + this.getNombre();
+  }
+}
--- src/entidad/unidad/medica/Urgencias.java
+++ src/entidad/unidad/medica/Urgencias.java
@@ -1,3 +1,37 @@
 package entidad.unidad.medica;
 
-public class Urgencias {}
+import enumerado.CodigoActividadEnum;
+import enumerado.CodigoAreaEnum;
+
+/** Subclase de {@link Medica} que representa una sala de urgencias del hospital. */
+public class Urgencias extends Medica{
+
+    /** Código de la actividad que se realiza en la unidad. */
+    private CodigoActividadEnum codigoActividad;
+
+    /**
+     * Constructor de Urgencias.
+     *
+     * @param nombre     nombre de la unidad.
+     */
+    public Urgencias(String nombre) {
+        super(CodigoAreaEnum.URGENCIAS, nombre);
+        this.codigoActividad = CodigoActividadEnum.ATENCION_URGENCIAS;
+    }
+
+    @Override
+    public CodigoActividadEnum getCodigoActividad() {
+        return this.codigoActividad;
+    }
+
+    @Override
+    public String toString() {
+        return this.getCodigoUnidad()
+                + "-"
+                + this.getCodigoUnidad()
+                + "-"
+                + this.getCodigoActividad()
+                + "-"
+                + this.getNombre();
+    }
+}
