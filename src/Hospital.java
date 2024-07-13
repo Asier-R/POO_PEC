ba54b7205ad34ac62d25a47405c07945eb55f2e2
Asier-R <monstruosaurio@gmail.com>
2024-07-13 09:03:45 +0200

    Prueba

--- src/Hospital.java
+++ src/Hospital.java
@@ -5,10 +5,7 @@ public class Hospital {
   public static void main(String[] args) {
     // Se genera javadoc con opciones: -encoding utf8 -docencoding utf8 -charset utf8
     System.out.println("ARRANQUE: Sistema Integrado de Gestión de un Hospital Universitario");
-
     LogicaTerminalDatos.iniciarLogicaTerminalDatos();
-
     System.out.println();
-
   }
 }
